45c390ce03f109b763a355b1a8ea6ebf166e4c86
MaxLocal <maxlocal@mail.ru>
2022-12-27 10:42:40 +0300

    Pass lesson-01 / works with modals

---
+++ src/js/modules/modals.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const modals = () => {
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    const trigger = document.querySelectorAll(triggerSelector),
+          modal = document.querySelector(modalSelector),
+          close = document.querySelector(closeSelector);
+
+    trigger.forEach(item => {
+      item.addEventListener('click', (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
+
+        modal.style.display = 'block';
+        // document.body.classList.add('modal-open');
+        document.body.style.overflow = 'hidden';
+      });
+    });
+
+    close.addEventListener('click', () => {
+      modal.style.display = 'none';
+      // document.body.classList.remove('modal-open');
+      document.body.style.overflow = '';
+    });
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        e.preventDefault();
+        modal.style.display = 'none';
+        // document.body.classList.remove('modal-open');
+        document.body.style.overflow = '';
+      }
+    });
+
+  }
+
+  function showModalByTime(selector, time) {
+    setTimeout(function() {
+      document.querySelector(selector).style.display = 'block';
+      document.body.style.overflow = 'hidden';
+    }, time);
+  }
+
+  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+  bindModal('.phone_link', '.popup', '.popup .popup_close');
+  showModalByTime('.popup', 3000);
+};
+
+export default modals;
