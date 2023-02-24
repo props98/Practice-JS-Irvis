6ca752afdee9fb271fad3185770b9a654e657443
MaxLocal <maxlocal@mail.ru>
2023-02-24 17:17:17 +0300

    Create docs directory for github pages

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-const dist = "./dist/";
+const dist = "./docs/";
 // const dist = "/Applications/MAMP/htdocs/Test";
 
 gulp.task("copy-html", () => {
